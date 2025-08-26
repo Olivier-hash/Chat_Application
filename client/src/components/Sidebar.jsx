4c2a83ac5af9ecff1892dae9c1d3486bfb807981
olivier Rukundo <rr.olivier01@gmail.com>
2025-08-26 22:15:00 +0200

    Implemented navigation after successful login using useNavigate

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -16,7 +16,7 @@ const navigate = useNavigate();
            <div className='relative py-2 group'>
             <img src={assets.menu_icon} alt="Menu" className='max-h-5 cursor-pointer'/>
             <div> 
-              <p className='cursor-pointer text-sm'>Edit Profile</p>
+              <p onClick={()=> navigate('/profile')} className='cursor-pointer text-sm'>Edit Profile</p>
               <hr className='my-2 border-t border-gray-500'/>
               <p className='cursor-pointer text-sm'>Logout</p>
             </div>
