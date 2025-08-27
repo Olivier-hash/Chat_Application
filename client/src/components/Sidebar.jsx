2dc529879740f802cf71f9d05c07eaf97a3d3aa7
olivier Rukundo <rr.olivier01@gmail.com>
2025-08-27 21:05:07 +0200

    Added profile menu with navigation in Sidebar

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -9,7 +9,7 @@ function Sidebar({selectedUser, setSelectedUser}) {
 const navigate = useNavigate();
 
   return (
-    <div>
+    <div className={`bg-[#8185B2]/10 h-full p-5 rounded-r-xl overflow-y-scroll text-white`}>
       <div className='pb-5'>
         <div className='flex justify-between items-center'>
            <img src={assets.logo} alt="Logo" className='max-w-40 '/>
