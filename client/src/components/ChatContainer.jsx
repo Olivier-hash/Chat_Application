2e0635dba6baa8015d269d00880e123728b23826
olivier Rukundo <rr.olivier01@gmail.com>
2025-09-01 22:08:52 +0200

    Added ChatContainer component with user selection feature

--- client/src/components/ChatContainer.jsx
+++ client/src/components/ChatContainer.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
 import assets from '../assets/assets'
-
-function ChatContainer() {
+                                                             // Props Destructuring
+function ChatContainer({selectedUser, setSelectedUser}) {    // added all setSelected userprops to be used onclick as a Function
+  
   return (
     <div>
       <div className='flex items-center gap-3 py-3 mx-4 border-b border-stone-500'>
@@ -11,7 +12,8 @@ function ChatContainer() {
           <span className='w-2 h-2 rounded-full bg-green-500'></span>
         </p>
         {/* Info icon and description */}
-        <img src={assets.arrow_icon} alt="" className='md:hidden max-w-7' />
+        <img onClick={()=> setSelectedUser(null)} src={assets.arrow_icon} alt="" className='md:hidden max-w-7' />
+
       </div>
     </div>
   )
