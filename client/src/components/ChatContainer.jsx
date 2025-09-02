0fb72c2637692a07ff47737ede125dc71fd989b6
olivier Rukundo <rr.olivier01@gmail.com>
2025-09-02 21:48:38 +0200

    Edited sender's ID

--- client/src/components/ChatContainer.jsx
+++ client/src/components/ChatContainer.jsx
@@ -19,22 +19,23 @@ function ChatContainer({selectedUser, setSelectedUser}) {    // added all setSel
       {/* ---------- chat area ---------- */}
       <div className='flex flex-col h-[calc(100%-120px)] overflow-y-scroll p-3 pb-6'>
         {messagesDummyData.map((msg, index)=>(
-          <div key={index} className={`flex items-end gap-2 justify-end ${msg.senderId !== '23873hns8992nnkjh42' && 'flex-row-reverse'}`}>
+          <div key={index} className={`flex items-end gap-2 justify-end ${msg.senderId !== '680f50e4f10f3cd28382ecf9' && 'flex-row-reverse'}`}>
             {msg.image ? (
               <img src={msg.image} alt="" className='max-w-[230px] border border-gray-700 rounded-lg overflow-hidden mb-8' />
             ): (
               <p className={`p-2 max-w-[200px] md:text-sm font-light rounded-lg mb-8 break-all bg-violet-500/30 text-white
-                 ${msg.senderId == '23873hns8992nnkjh42' ? 'rounded-br-none' : 'rounded-bl-none' }`}>{msg.text}
+                 ${msg.senderId == '680f50e4f10f3cd28382ecf9' ? 'rounded-br-none' : 'rounded-bl-none' }`}>{msg.text}
               </p>
             )}
             {/* ID: 680f50e4f10f3cd28382ecf9 */}
             <div>
-              <img src={msg.senderId ==='23873hns8992nnkjh42' ? 
+              <img src={msg.senderId ==='680f50e4f10f3cd28382ecf9' ? 
               assets.avatar_icon : assets.profile_martin} alt="" className='w-7 rounded-full'/>
               <p className='text-gray-500'>{msg.createdAt}</p>
             </div>
           </div>
         ))}
+        {/* Reference */}
       </div>
     </div>
   ) : (
