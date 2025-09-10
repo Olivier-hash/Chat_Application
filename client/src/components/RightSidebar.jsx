6ed79622079c51673b286e6ea432afc51d92755e
olivier Rukundo <rr.olivier01@gmail.com>
2025-09-10 22:37:34 +0200

    Added image for profile pic on Rightsidebar

--- client/src/components/RightSidebar.jsx
+++ client/src/components/RightSidebar.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import assets from '../assets/assets'
 
 function RightSidebar({selectedUser}) { // destructurung selected user from the props 
   return selectedUser && (
     <div>
-      
+      <div>
+        <img src={selectedUser?.profilePic || assets.avatar_icon} alt="" />
+      </div>
     </div>
   )
 }
