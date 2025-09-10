5d65356dfaa1e22ce444570e025bb0e57db96730
olivier Rukundo <rr.olivier01@gmail.com>
2025-09-10 22:41:01 +0200

    Added name description for user also on Right sideBar

--- client/src/components/RightSidebar.jsx
+++ client/src/components/RightSidebar.jsx
@@ -5,7 +5,9 @@ function RightSidebar({selectedUser}) { // destructurung selected user from the 
   return selectedUser && (
     <div>
       <div>
-        <img src={selectedUser?.profilePic || assets.avatar_icon} alt="" />
+        <img src={selectedUser?.profilePic || assets.avatar_icon} alt=""
+        className='w-20 aspect-[1/1] rounded-full' />
+        <h1>{selectedUser.fullName}</h1>
       </div>
     </div>
   )
