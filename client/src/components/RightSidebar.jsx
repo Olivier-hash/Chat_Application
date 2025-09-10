6358b1b624c232fea764c34410989e2cdb7e7a41
olivier Rukundo <rr.olivier01@gmail.com>
2025-09-10 22:45:27 +0200

    Created RightSidebar with styled avatar and name using Tailwind CSS

--- client/src/components/RightSidebar.jsx
+++ client/src/components/RightSidebar.jsx
@@ -7,7 +7,9 @@ function RightSidebar({selectedUser}) { // destructurung selected user from the 
       <div>
         <img src={selectedUser?.profilePic || assets.avatar_icon} alt=""
         className='w-20 aspect-[1/1] rounded-full' />
-        <h1>{selectedUser.fullName}</h1>
+        <h1 className='px-10 text-xl font-medium mx-auto flex items-center gap-2'>
+          {selectedUser.fullName}
+        </h1>
       </div>
     </div>
   )
