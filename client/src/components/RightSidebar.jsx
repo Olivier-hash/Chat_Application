c0b091b705181d45b76c59946afe545f3a3f9760
olivier Rukundo <rr.olivier01@gmail.com>
2025-09-11 21:34:06 +0200

    Added tailwind class for right sidebar  profile-pic

--- client/src/components/RightSidebar.jsx
+++ client/src/components/RightSidebar.jsx
@@ -3,8 +3,8 @@ import assets from '../assets/assets'
 
 function RightSidebar({selectedUser}) { // destructurung selected user from the props 
   return selectedUser && (
-    <div>
-      <div>
+    <div >
+      <div className='pt-16 flex flex-col items-center gap-2 text-xs font-light mx-auto'>
         <img src={selectedUser?.profilePic || assets.avatar_icon} alt=""
         className='w-20 aspect-[1/1] rounded-full' />
         <h1 className='px-10 text-xl font-medium mx-auto flex items-center gap-2'>
