ef4fb2faac81e156bff7f6ac3cfed725d9e772f9
olivier Rukundo <rr.olivier01@gmail.com>
2025-09-11 21:50:54 +0200

    Added git Responsiveness to show profile pic for different screen

--- client/src/components/RightSidebar.jsx
+++ client/src/components/RightSidebar.jsx
@@ -3,7 +3,10 @@ import assets from '../assets/assets'
 
 function RightSidebar({selectedUser}) { // destructurung selected user from the props 
   return selectedUser && (
-    <div >
+  
+    <div className={`bg-[#818582]/10 text-white w-full relative overflow-y-scroll
+    ${selectedUser ? "max-md:hidden" : ""}`}> 
+    
       <div className='pt-16 flex flex-col items-center gap-2 text-xs font-light mx-auto'>
         <img src={selectedUser?.profilePic || assets.avatar_icon} alt=""
         className='w-20 aspect-[1/1] rounded-full' />
@@ -15,6 +18,7 @@ function RightSidebar({selectedUser}) { // destructurung selected user from the 
         </h1>
         <p className='px-10 mx-auto'>{selectedUser.bio}</p>
       </div>
+
     </div>
   )
 }
