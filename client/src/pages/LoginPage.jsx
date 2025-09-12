b3265dff2160ff58350faaac439bf1d5e2d1b3f9
olivier Rukundo <rr.olivier01@gmail.com>
2025-09-12 22:45:45 +0200

    applied backdrop blur and flex layout to LoginPage

--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
+import assets from '../assets/assets'
 
 function LoginPage() {
   return (
     <div className='min-h-screen bg-cover bg-center flex items-center justify-center gap-8 sm:justify-evenly
     mx-sm:flex-col backdrop-blur-2xl'>
 
+      {/* ----------Left------------ */}
+      <img src={assets.logo_big} alt="" className='w-[min(30vw,250px)]'/>
+
+      {/* ----------right------------ */}
+
+      <form className=''>
+
+      </form>
     </div>
   )
 }
