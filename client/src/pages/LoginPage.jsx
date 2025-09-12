d3c7fa159c4cde46e7321a422cbcfc5f1606631d
olivier Rukundo <rr.olivier01@gmail.com>
2025-09-12 22:39:55 +0200

    created LoginPage with full-screen layout and flex alignment

--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -2,7 +2,10 @@ import React from 'react'
 
 function LoginPage() {
   return (
-    <div>Login Page</div>
+    <div className='min-h-screen bg-cover bg-center flex items-center justify-center gap-8 sm:justify-evenly
+    mx-sm:flex-col backdrop-blur-2xl'>
+
+    </div>
   )
 }
 
