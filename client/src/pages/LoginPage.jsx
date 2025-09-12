4f177f52ddd16f30629cb2eda762520c6d11e1d4
olivier Rukundo <rr.olivier01@gmail.com>
2025-09-12 22:51:05 +0200

    Added state variables

--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -1,17 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
 import assets from '../assets/assets'
 
 function LoginPage() {
+
+// STATE VARIABLE
+
+const [currState, setCurrState] = useState("Sign up")
+
+
+
   return (
-    <div className='min-h-screen bg-cover bg-center flex items-center justify-center gap-8 sm:justify-evenly
-    mx-sm:flex-col backdrop-blur-2xl'>
+    <div className='min-h-screen bg-cover bg-center flex items-center 
+    justify-center gap-8 sm:justify-evenly mx-sm:flex-col backdrop-blur-2xl'>
 
       {/* ----------Left------------ */}
       <img src={assets.logo_big} alt="" className='w-[min(30vw,250px)]'/>
 
       {/* ----------right------------ */}
 
-      <form className=''>
+      <form className='border-2 bg-white/8 text-white border-gray-500 p-6 
+      flex flex-col gap-6 rounded-lg shadow-lg'>
 
       </form>
     </div>
