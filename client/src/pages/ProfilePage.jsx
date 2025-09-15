945bb39a1234a2a1f625fb5b29cd06e5a08a6874
olivier Rukundo <rr.olivier01@gmail.com>
2025-09-15 22:33:22 +0200

    Added state variables and usenavigator

--- client/src/pages/ProfilePage.jsx
+++ client/src/pages/ProfilePage.jsx
@@ -1,6 +1,11 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
 
 function ProfilePage() {
+
+  const [selectedImg, setSelectedImg] = useState(null)
+  const navigate = useNavigate()
+
   return (
     <div className='min-h-screen bg-cover bg-no-repeat flex items-center justify-center'>
       <div className='w-5/6 max-w-2xl backdrop-blur-2xl text-gray-300 border-2 border-gray-600
@@ -10,6 +15,7 @@ function ProfilePage() {
           <h3 className='text-lg'>Profile details</h3>
           <label htmlFor="avatar" className='flex items-center gap-3 cursor-pointer'>
             <input type="file" id='avatar' accept='.png, .jpg, .jpeg hidden'/>
+            <img src={""} alt="" />
           </label>
         </form>
         <img src="" alt="" />
