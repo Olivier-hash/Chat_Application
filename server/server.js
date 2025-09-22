a9d7dcc3118fcaf67d8976a35287280ff90b0b38
olivier Rukundo <rr.olivier01@gmail.com>
2025-09-22 15:53:58 +0200

    Added function to connect mongoDB database

--- server/server.js
+++ server/server.js
@@ -2,6 +2,8 @@ import express from "express";
 import "dotenv/config";
 import cors from "cors";
 import http from "http";
+import { connect } from "http2";
+import { connectDB } from "./lib/db.js";
 
 
 // create Express app and HTTP Server
@@ -13,6 +15,11 @@ app.use(express.json({limit:"4mb"}));
 app.use(cors());     // CORS = Cross-Origin Resource Sharing.
 
 app.use("/api/status", (req,res)=> res.send("Server is live"));
+
+
+// connect to mongoDB
+await connectDB();
+
 
 const PORT = process.env.PORT || 5000;
 server.listen(PORT, ()=> console.log(`server is running on PORT: ${PORT}`));
