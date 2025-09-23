155680ddf403393453abda313989c57fbffd8d03
olivier Rukundo <rr.olivier01@gmail.com>
2025-09-23 22:57:04 +0200

    added Cloudinary setup for image uploads

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,6 +1,7 @@
 import { generateToken } from "../lib/utils.js";
 import User from "../models/User.js";
 import bcrypt from "bcryptjs"
+import cloudinary from "../lib/cloudinary.js";
 
 // Signup a new user
 export const signup = async (req,res)=>{
@@ -62,5 +63,32 @@ export const login = async (req, res) => {
          console.log(error.message);
         res.json({success: false, message: error.message})
   
+    }
+}
+
+// Controller to check if user is authenticated
+export const checkAuth = (req, res)=> {
+    res.json({ success:true, user: req.user })
+}
+
+
+// Controller to update user profile details
+export const updateProfile = async (req,res)=> {
+    try {
+        const { profilePic, bio, fullName } = req.body;
+
+        const userId = req.user._id
+        let updatedUser;
+
+        if (!profilepic) {
+           updatedUser = await User.findByIdAndUpdate(userId, {bio, fullName},
+           {new:true});
+        }else{
+            const upload = await cloudinary.uploader.upload(profilePic);
+
+            updatedUser = await User.findByIdAndUpdate(userId, {profilePic: upload.secure_url})
+        }
+    } catch (error) {
+        
     }
 }
--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -18,6 +18,13 @@ export const protectRoute = async (req, res, next)=> {
         req.user = user
         next();
     } catch (error) {
+        console.log(error,message);
         res.json({ success:false, message: error.message })
     }
 }
+
+// Controller to check if user is authenticated
+export const checkAuth = (req, res)=>{
+    res.json({success: true, user: req.user});
+}
+
