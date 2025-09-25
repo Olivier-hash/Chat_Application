8f5deeb0bcc45cf1826ff42e77948f5cc4603d25
olivier Rukundo <rr.olivier01@gmail.com>
2025-09-25 13:28:55 +0200

    Added getUserForSidebar controller with unseen message count functionality

--- server/controllers/messageController.js
+++ server/controllers/messageController.js
@@ -1,6 +1,7 @@
 import Message from "../models/Message.js";
 import User from "../models/User.js";
 
+
 // Get all users except the logged in user
 export const getUserForSidebar = async (req,res)=> {
     try {
@@ -13,8 +14,14 @@ export const getUserForSidebar = async (req,res)=> {
         const promises = filteredUsers.map(async (user)=>{
             const messages = await Message.find({senderId: user._id, receiverId:
             userId, seen: false})
+            if (messages.length > 0) {
+                unseenMessage[user._id] = messages.length
+            }
         })
+        await Promise.all(promises);
+        res.json({ success: true, users: filteredUsers, unseenMessage})
     } catch (error) {
-        
+        console.log(error.message);
+        res.json({success: false, message: error.message})
     }
 }
