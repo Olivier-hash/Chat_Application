c8d3a8a93acb3e336873e66d8b3505508fdd7787
olivier Rukundo <rr.olivier01@gmail.com>
2025-09-28 18:15:32 +0200

    Fixed error from created users not displaying

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,12 +2,15 @@ import { createRoot } from 'react-dom/client'
 import {BrowserRouter} from 'react-router-dom'
 import './index.css'
 import App from './App.jsx'
-import { AuthContext, AuthProvider } from '../context/AuthContext.jsx'
+import { AuthProvider } from '../context/AuthContext.jsx'
+import { ChatProvider } from '../context/chatContext.jsx'
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
   <AuthProvider>
-    <App />
+    <ChatProvider>
+       <App />
+    </ChatProvider>
   </AuthProvider>
   </BrowserRouter>,
 )
