3a1503287bccf1e2c6566ce3052004d6030b98cb
olivier Rukundo <rr.olivier01@gmail.com>
2025-10-02 23:47:48 +0200

    Integrated real-time messaging with socket.io

--- client/context/ChatContext.jsx
+++ client/context/ChatContext.jsx
@@ -11,7 +11,7 @@ export const ChatProvider = ({ children })=>{
     const [messages, setMessages] = useState([]);
     const [users, setUsers] = useState([]);
     const [selectedUser, setSelectedUser] = useState(null)
-    const [unseenMessages, setUnseenMessages] = useState({})
+
     //
 
     const {socket, axios} = useContext(AuthContext);
