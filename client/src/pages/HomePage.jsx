80084ae27429ecc20830e1696701dc569eb93be0
olivier Rukundo <rr.olivier01@gmail.com>
2025-10-04 23:00:03 +0200

    Replaced props in Home.jsx with useContext

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -16,8 +16,8 @@ function HomePage() {
       <div className={`backdrop-blur-xl border-2 border-gray-600 rounded-2xl 
         overflow-hidden h-[100%] grid grid-cols-1 relative ${selectedUser  ? 'md:grid-cols-[1fr_1.5fr_1fr] xl:grid-cols-[1fr_2fr_1fr]' : 'md:grid-cols-2'}`}>
         <Sidebar/> 
-        <ChatContainer selectedUser={selectedUser} setSelectedUser={setSelectedUser}/>  {/* Added props inorder to render selected user */}
-        <RightSidebar selectedUser={selectedUser} setSelectedUser={setSelectedUser}/>   {/* same as here */}
+        <ChatContainer selectedUser={selectedUser} setSelectedUser={setSelectedUser}/>  {/* Added props inorder to render selected user && updated code after replaced */}
+        <RightSidebar selectedUser={selectedUser} setSelectedUser={setSelectedUser}/>   {/* same as here  */}
       </div>
     </div>
   ) 
