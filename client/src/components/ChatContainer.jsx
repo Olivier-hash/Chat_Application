71e2d745268dc3ddd7100f3c19755d894f26f56b
olivier Rukundo <rr.olivier01@gmail.com>
2025-10-18 23:03:23 +0200

    hidden status green color icon for offline users

--- client/src/components/ChatContainer.jsx
+++ client/src/components/ChatContainer.jsx
@@ -58,8 +58,8 @@ function ChatContainer() {    // added all setSelected userprops to be used oncl
         <img src={selectedUser.profilePic || assets.avatar_icon } alt="" className='w-8 rounded-full' />
         <p className='flex-1 text-lg text-white flex items-center gap-2'>           
           {selectedUser.fullName}
-          {onlineUsers.includes(selectedUser._id)}
-          <span className='w-2 h-2 rounded-full bg-green-500'></span>
+          {onlineUsers.includes(selectedUser._id) &&
+          <span className='w-2 h-2 rounded-full bg-green-500'></span>}
         </p>
         {/* Info icon and description */}
         <img onClick={()=> setSelectedUser(null)} src={assets.arrow_icon} alt="" className='md:hidden max-w-7' />
